42eef01c92b4a1f294fb335a5a148ff5294550bc
vern.li <vern.li@herely.us>
2018-10-04 22:27:06 +0800

    :rocket: 区分生产环境和本定环境中django的DEBUG

--- thtr/settings.py
+++ thtr/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '4wj84r8)%d-#fjw(xr!!$bcm$ewv)ka7=i(0)$^3*qlv=%-)jh'
+SECRET_KEY = os.getenv('SECRET_KEY', SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.getenv('MODE', 'local') == 'local')
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
