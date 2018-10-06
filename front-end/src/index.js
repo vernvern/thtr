356a64b7f9b3853e2edf60c34f5de5bcec340335
vern.li <vern.li@herely.us>
2018-10-06 16:59:11 +0800

    :heavy_plus_sign: react-router

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Router, Route, Switch } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker';
+import App from './App';
+import history from './history';
+import Login, { Register } from './login';
+
+
+ReactDOM.render(
+  <Router history={history}>
+    <Switch>
+      <Route exact path='/' component={App} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ front-end/src/login.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="Login">
+        <header className="App-header">
+        <a> Login test </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export class Register extends Component {
+  render() {
+    return (
+      <div className="Register">
+        <header className="App-header">
+        <a> Register test </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+
+export default Login;
+
