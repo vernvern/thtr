a580abad3c75e516daa0ba4be7ee16e2e1f3b518
vern.li <vern.li@herely.us>
2018-10-07 22:15:24 +0800

    :sparkles: /login

--- front-end/src/Views/Login.js
+++ front-end/src/Views/Login.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 
 class Login extends Component {
   render() {
+
+    const spaceStyle = {
+      height: '150px',
+    };
+
+    const loginFormStyle = {
+      border: '1px solid #c1c0bd',
+    };
+
     return (
-      <div className="Login">
-        <a> Login test </a>
-        <button className="btn-large">Large</button>
+      <div id='login'>
+
+        <div className='row' style={spaceStyle} />
+
+        <div className="row flex-center">
+          <div className=''>
+
+            <div className="row">
+              <p> Logo </p>
+            </div>
+
+            <div className="row flex-space child-borders" >
+              <div className="sm-12 md-12 col padding-left-large">
+                <div className="form-group">
+                    <label htmlFor="account">账号</label>
+                    <input type="text" placeholder="邮箱 | 账号" id="account" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">密码</label>
+                    <input type="text" placeholder="🙈" id="password" />
+                </div>
+
+                <button type='submit' className='btn-block'> 登录 </button>
+              </div>
+            </div>
+
+            <div className="row flex-space child-borders" >
+              <div className="sm-12 md-12 col padding-smal">
+                <p>没有thtr账号？<Link to='/register' style={{ color: '#0066CC', textDecorationColor:'#ffffff',textDecorationLine: "underline",textDecorationStyle: "solid" }}>注册一个</Link></p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
       </div>
     );
   }
