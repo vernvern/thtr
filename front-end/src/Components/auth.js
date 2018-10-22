2c6e7ea6e497555324521f2c6c7105a228e188a0
vern.li <vern.li@herely.us>
2018-10-22 20:27:42 +0800

    :sparkles: localStorage demo

--- front-end/src/Components/auth.js
+++ front-end/src/Components/auth.js
@@ -6,16 +6,20 @@ class Auth extends React.Component {
     super(props);
 
     this.login = this.login.bind(this);
-    this.isLogin = false;
+    this.logout = this.logout.bind(this);
+    var access_token = localStorage.getItem('access_token');
+    this.isLogin = access_token ? true : false;
   }
 
   login(e) {
     this.isLogin = true;
+    localStorage.setItem('access_token', '2333');
     return this.isLogin;
   }
 
   logout(e) {
     this.isLogin = false;
+    localStorage.removeItem('access_token');
     return this.isLogin;
   }
 
