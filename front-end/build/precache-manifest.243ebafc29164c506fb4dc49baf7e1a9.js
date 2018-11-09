392b80971b5b843ed68f549e646766813350435b
vern.li <vern.li@herely.us>
2018-11-09 14:43:44 +0800

    :rocket: npm run build

---
+++ front-end/build/precache-manifest.243ebafc29164c506fb4dc49baf7e1a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "419da4d2514988e98b2a",
+    "url": "/static/js/main.419da4d2.chunk.js"
+  },
+  {
+    "revision": "e4f4352fbad9ee6d46cf",
+    "url": "/static/js/1.e4f4352f.chunk.js"
+  },
+  {
+    "revision": "419da4d2514988e98b2a",
+    "url": "/static/css/main.e706c7f8.chunk.css"
+  },
+  {
+    "revision": "c0865f8100ba9fbe30d86a5562b8b7ab",
+    "url": "/index.html"
+  }
+];
